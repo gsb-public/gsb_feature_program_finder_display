61aff7eb5cd4d99877b23bf97b9433e51ea45f03
Ayuta Padhi <apadhi@stanford.edu>
2016-01-27 13:57:21 -0800

    WP-6461 Added program format to program listing

--- templates/program-isotope-item.tpl.php
+++ templates/program-isotope-item.tpl.php
@@ -1,14 +1,34 @@
+<!--<div class="program-title" style="padding-right:300px">
+  <?php //print $title; ?>
+</div>-->
 <div class="program-title">
   <?php print $program_title; ?>
 </div>
-<?php if($start_date != '' && $end_date != '' ) { ?>
+<?php if($start_date != '' && $end_date != '' && $date_select == 'Date') { ?>
   <div class="program-date">
-    <?php print date_format(date_create($start_date),'d M Y').' – '.date_format(date_create($end_date),'d M Y'); ?>
+    <?php print date_format(date_create($start_date),'d M Y').' – '.date_format(date_create($end_date),'d M Y') ; ?>
   </div>
 <?php } ?>
-<?php if ($program_location != '') { ?>
-  <div class="program-location">
-    <?php print $program_location; ?>
+<?php if($date_select == 'Text' && $text_date != '' ) { ?>
+  <div class="program-text-date">
+    <?php print $text_date; ?>
+  </div>
+<?php } ?>
+<?php //if ($program_location != '') { ?>
+  <!--<div class="program-location">
+    <?php //print $program_location; ?>
+  </div>-->
+<?php// } ?>
+<?php $location = $program_format;
+      if ($location != '' &&  $location_name != '') {
+       $location .= ' | ' . $location_name;
+      } else {
+       $location .= $location_name;
+      }
+      if ($location != '') {
+       ?>
+  <div class="program-format">
+    <?php print $location ?>
   </div>
 <?php } ?>
 <div class="program-teaser">
