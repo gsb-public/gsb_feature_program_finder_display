4234d578a6ee6726d03078c3e74f391b8f48f22c
Meghan Plunkett <meghanp@stanford.edu>
2016-02-10 12:00:30 -0500

    WP-6557: add wrapper for full time program item.

--- templates/program-isotope-item-wrapper.tpl.php
+++ templates/program-isotope-item-wrapper.tpl.php
@@ -2,9 +2,11 @@
   <div class="programs">
     <?php print $programs; ?>
     <h3 class="full-time-header"><?php print t('Full-Time Degree Programs'); ?></h3>
-    <div class="full-time-programs">
+    <div class="full-time-programs-wrapper">
       <?php foreach ($full_time_programs as $it => $itd): ?>
-        <?php print $itd['data']; ?>
+        <div class="full-time-program">
+          <?php print $itd['data']; ?>
+        </div>
       <?php endforeach; ?>
     </div>
     <div class="image-ctas">
