6eed2f83a9ba3acfef9df969e889ded2323677d9
Roopesh Naik <rnaik77@stanford.edu>
2016-05-13 15:35:47 +0530

    WP-7169 remove the pipe found at end in application deadlines

--- js/gsb_program_compare.js
+++ js/gsb_program_compare.js
@@ -4,6 +4,14 @@
       $(".views-field-field-brochure-pdf #apower-pdf").each(function(){
           $(this).siblings().hide();
       });
+
+      // remove the pipe if found at the end for application deadlines
+      $(".field-collection-item-field-application-deadlines").each(function(){
+        var text = $(this).text();
+        if (text.substr(-1) === '|') {
+          $(this).text($(this).text().replace('|',''));
+        }
+      });
     }
   };
 })(jQuery);
