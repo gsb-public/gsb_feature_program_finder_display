759a3c3143e08013c6385a9591c64eebd470a59a
Ayuta Padhi <apadhi@stanford.edu>
2016-06-02 14:45:29 -0700

    WP-7200 program finder enhancement implementation

--- templates/program-isotope-degree-item.tpl.php
+++ templates/program-isotope-degree-item.tpl.php
@@ -2,7 +2,7 @@
 <div class="full-time-programs-wrapper">
   <?php if (!empty($full_time_programs)): ?>
     <?php foreach ($full_time_programs as $it => $itd): ?>
-      <div class="full-time-program">
+      <div class="full-time-program " data-daterange="99999999" data-sort-start_date="99999999">
         <?php print $itd['data']; ?>
       </div>
     <?php endforeach; ?>
